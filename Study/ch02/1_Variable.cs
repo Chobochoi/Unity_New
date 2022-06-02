fd0498d1e8fb2d6aa5cef661c6268df441ff25ac
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-02 15:02:12 +0900

    2022/06/22 수업예제

---
+++ Study/ch02/1_Variable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 2022/06/02
+이름 최미수
+내용 C# 변수 실습 하기 교재 p.79
+*/
+
+/* 변수(Variable) : 데이터 처리를 위해 데이터를 저장하는 데이터 그릇
+ * 여러번 데이터를 변경할 수 있음
+ * 
+ * 
+ * 상수(Constant) : 데이터를 변경할 수 없는 데이터*/
+
+namespace Ch02
+{
+    internal class _1_Variable
+    {
+        static void Main1(string[] args)
+        {
+            ///////////////////
+            // 변수
+            ///////////////////
+            int num1; // 선언
+            num1 = 1; // 대입
+
+            int num2 = 2; // 선언과 초기화
+
+            int num3 = num1 + num2; // 연산
+
+            Console.WriteLine("num3 : "+num3); // 결과값
+
+
+            ///////////////////
+            // 상수
+            ///////////////////
+
+            const int NUM = 10;
+            const double PI = 3.141592;
+            //num = 20;
+            //num = 30;
+
+            Console.WriteLine("NUM :" +NUM);
+            Console.WriteLine("PI :" +PI);
+
+        }
+    }
+}
---
+++ Study/ch02/3_Operator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 2022/06/02
+이름 최미수
+내용 연산자(Operator) 실습하기 교재 p.90
+*/
+
+
+namespace Ch02
+{
+    internal class _3_Operator
+    {
+        static void Main(string[] args)
+        {
+            // 산술 연산자
+            // 증가, 감소 연산자
+            // 복합 대입 연산자
+            // 논리 연산자
+            // 조건 연산자
+        }
+    }
+}
