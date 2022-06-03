ab4577712ffae8d88addaa291318fc3996508a5b
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-03 12:19:31 +0900

    Unity_New_Day2
    If
    Switch

--- Study/Ch03/2_Switch.cs
+++ Study/Ch03/2_Switch.cs
@@ -4,9 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+날짜 2022/06/03
+이름 최미수
+내용 Switch 실습 교재 p.127
+*/
+
 namespace Ch03
 {
     internal class _2_Switch
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("점수 입력 :");
+            string strScore = Console.ReadLine();
+
+            int score = int.Parse(strScore);
+            int grade = score/10;
+
+            switch (grade)
+            {
+                case 10:
+                case 9:
+                    Console.WriteLine("A입니다.");
+                    break;
+
+                case 8:
+                    Console.WriteLine("B입니다.");
+                    break;
+
+                case 7:
+                    Console.WriteLine("C입니다.");
+                    break;
+
+                case 6:
+                    Console.WriteLine("D입니다.");
+                    break;
+                default:
+                    Console.WriteLine("F입니다.");
+                    break;
+            }    
+
+
+        }
     }
 }
