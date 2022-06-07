a565d001c781bc6be20036de6c7d6375b422ca5a
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-07 11:17:18 +0900

    Unity_New_Day3
    For
    While

--- Study/Ch03/3_For.cs
+++ Study/Ch03/3_For.cs
@@ -4,9 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+날짜 2022/06/07
+이름 최미수
+내용 반복문(For) 실습 교재 p. 157
+*/
+
 namespace Ch03
 {
     internal class _3_For
     {
+        static void Main1(string[] args)
+        {
+            // for 문
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("{0}회 반복...", i);
+            }
+            // 1부터 10까지 합
+            int sum = 0;
+
+            for(int k = 1; k <= 10; k++)
+            {
+                sum += k; // sum = sum + k
+            }
+            Console.WriteLine("1부터 10까지의 합 :"+sum);
+
+            // 1부터 10까지 짝수 합
+            int tot = 0;
+            for(int k =1; k <= 10; k++)
+            {
+                if (k%2 == 0)
+                {
+                    tot += k;
+                }
+
+            }
+            Console.WriteLine("1부터 10까지의 짝수 합 :" +tot);
+
+            // 중첩 for 문
+            for(int a = 1; a <= 3; a++)
+            {
+                Console.WriteLine("a :"+a);
+                
+                for(int b= 1; b <= 4; b++)
+                {
+                    Console.WriteLine("b :" +b);
+                    
+                }
+            }
+
+            // 구구단
+            for(int x = 2; x <= 9; x++)
+            {
+                Console.WriteLine("{0}단 시작", x);
+                for(int y = 1; y <= 9; y++)
+                {
+                    int z = x * y;
+                    Console.WriteLine("{0} x {1} = {2}",x , y, z);
+                }
+            }
+
+            // 별삼각형
+            for(int start = 1; start <= 10; start++)
+            {
+                for(int end =10; end >= start; end--)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine(); // 줄 바꿈
+            }
+        }
     }
 }
--- Study/Ch03/4_While.cs
+++ Study/Ch03/4_While.cs
@@ -4,9 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+날짜 2022/06/07
+이름 최미수
+내용 반복문(While) 실습 교재 p. 162
+*/
+
 namespace Ch03
 {
     internal class _4_While
     {
+        static void Main1(string[] args)
+        {
+            // 1부터 10까지 합
+            int sum = 0;
+            int k = 1;
+            
+            while(k <= 10) // while(조건식)
+            {
+                sum += k;
+                k++;
+            }
+            Console.WriteLine("1부터 10까지의 합 :" +sum);
+
+            // do ~ while : 최초 1번은 무조건 반복 실행하는 반복문
+            // 짝수 합을 구하시오
+
+            int total = 0;
+            int i = 1;
+            do
+            {
+                if (i% 2 == 0)
+                {
+                    total += i;                    
+                }
+                i++;
+            } 
+            while (i <= 10);
+
+            Console.WriteLine("1부터 10까지 짝수 합 : "  +total) ;
+
+            // break
+            int num = 1;
+
+            while (true)
+            {
+                if (num % 5 == 0 && num % 7 == 0)
+                {
+                    break; // 반복문 종료
+                }
+                num++;
+            }
+
+            Console.WriteLine("5와 7의 최소공배수 :" +num);
+
+            // continue
+            int tot = 0;
+            int j = 0;
+
+            while(j <= 10)
+            {
+                j++;    
+
+                if(j % 2 == 1)
+                {
+                    continue; // 반복문의 처음으로 이동
+                }
+                tot += j;
+
+            }
+
+            Console.WriteLine("1부터 10까지 짝수합 :" +tot);
+        }
     }
 }
