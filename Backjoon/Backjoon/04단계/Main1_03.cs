c9b972a92f8ccacd0581399904254f818f59488a
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-15 17:17:11 +0900

    BackJoon

--- Backjoon/Backjoon/04단계/Main1_03.cs
+++ Backjoon/Backjoon/04단계/Main1_03.cs
@@ -8,8 +8,39 @@ namespace Backjoon._04단계
 {
     internal class Main1_03
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
+            int[] input = new int[3];
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+
+           
+            int temp = input[0] * input[1] * input[2];
+
+           
+            char[] pieceNum = temp.ToString().ToCharArray();
+
+           
+            int count = 0;
+
+           
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < pieceNum.Length; j++)
+                {
+                    if (pieceNum[j].ToString() == i.ToString())
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                count = 0;
+
+            }
 
         }
     }
---
+++ Backjoon/Backjoon/04단계/Main1_04.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon._04단계
+{
+    internal class Main1_04
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[10];
+            int result = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+                input[i] = input[i] % 42 ;
+            }
+
+            for(int i =0; i < input.Length; i++)
+            {
+                int count = 0;
+                for(int j = i+1; j< input.Length; j++)
+                {
+                    if(input[j] == input[i])
+                          count++;
+                    
+                   
+                }
+                if (count == 0) result++;
+
+                
+            }
+
+            Console.WriteLine(result);
+
+            
+
+
+
+        }
+    }
+}
