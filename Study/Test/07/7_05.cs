d7a47ed71d9f30588f1d6f85693b47c83d0d635d
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-27 16:17:20 +0900

    Test7

---
+++ Study/Test/07/7_05.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._07
+{
+    internal class _7_05
+    {
+        static void Main1(string[] args)
+        {
+            int first = 10; 
+            int second = 20;
+            bool result = true;
+
+            result = (first == second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = (first < second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = (first >= second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = (first != second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = (first == second) || (first > 5);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = (first == second) && (first > 5);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+
+            result = true ^ false;
+            Console.WriteLine("{0} ^ {1} : {2}",true, false , result);
+        }
+    }
+}
---
+++ Study/Test/07/7_09.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._07
+{
+    internal class _7_09
+    {
+        enum Size { Short, Tall, Grande, Venti };
+        enum Coffe { Short = 3300, Tall = 3800, Grande = 4300, Venti =4800 };
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("커피 가격표");
+
+            for (int i = 0; i<4; i++)
+            {
+                if (i == (int)Size.Short)
+                {
+                    Console.WriteLine("{0,10}:{1:C}",Size.Short, Convert.ToInt32(Coffe.Short));
+                    //tall grande venti 순서로
+                }
+            }
+
+        }
+    }
+}
